737d89bc7a76b361996b27bb2834131b97865cb0
Luiz Fernando <luizfernando.lf355@gmail.com>
2022-12-17 20:23:51 -0300

    criação do back-end com python

---
+++ app/cliente.py
@@ -0,0 +1,10 @@
+class Cliente:
+    def __init__(self, nome, contato):
+        self.nome = nome
+        self.contato = contato
+
+    def comprar(self):
+        pass
+
+    def pagar(self):
+        pass
