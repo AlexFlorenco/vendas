9f7352fe8ebe2b39fd4b0a7bb10e35bb46131330
Luiz Fernando <luizfernando.lf355@gmail.com>
2023-01-08 03:27:48 -0300

    gerador de arquivos csv

--- app/exp_tests/__init__.py
+++ app/exp_tests/__init__.py
@@ -2,8 +2,9 @@ from app.exp_tests import data_frame_cliente as dfc
 import pandas as pd
 
 if __name__ == "__main__":
-    path_name = dfc.criar_cliente_csv()
-    df = pd.read_csv(path_name)
-    cleito = dfc.gerar_dados_aleatorios()
-    df = dfc.inserir_row_cliente(cleito, df)
-    print(df)
+    cleito = dfc.gerar_cliente_aleatorio(1)
+    # cleito2 = dfc.gerar_cliente_aleatorio(2)
+    df_cliente = dfc.inserir_row_cliente(cleito)
+    # df_cliente = dfc.inserir_row_cliente(cleito2, df_cliente)
+    print(df_cliente)
+
--- app/exp_tests/data_frame_cliente.py
+++ app/exp_tests/data_frame_cliente.py
@@ -1,5 +1,4 @@
 from datetime import date
-
 import pandas as pd
 import numpy as np
 import os
@@ -8,37 +7,90 @@ from app.produto import Produto
 
 
 def criar_cliente_csv():
-    if os.path.exists("clientes.csv"):
-        return "clientes.csv"
-
-    return new_csv()
-
-
-def new_csv():
-    df = pd.DataFrame(columns=['nome', 'contato', 'total_comprado', 'total_pago', 'id_compras', 'id_pagamentos'])
-    df.to_csv("clientes.csv")
-    return "clientes.csv"
+    if os.path.exists("armazem/clientes.csv"):
+        return criar_compra_csv()
+    df = pd.DataFrame(columns=['nome', 'contato', 'total_comprado', 'total_pago'])
+    df.to_csv("armazem/clientes.csv")
+    criar_compra_csv()
 
 
-def inserir_row_cliente(cliente, df):
+def criar_compra_csv():
+    if os.path.exists("armazem/compras.csv"):
+        return criar_pagamento_csv()
+    df = pd.DataFrame(columns=['id_cliente', 'nome_produto', 'valor_produto', 'descricao_produto',
+                               'data', 'is_debito_imediato'])
+    df.to_csv("armazem/compras.csv")
+    criar_pagamento_csv()
+
+
+def criar_pagamento_csv():
+    if os.path.exists("armazem/pagamentos.csv"):
+        return
+    df = pd.DataFrame(columns=['id_cliente', 'valor', 'data'])
+    df.to_csv("armazem/pagamentos.csv")
+
+
+def inserir_row_cliente(cliente):
+    criar_cliente_csv()
+    df_cliente = pd.read_csv('armazem/compras.csv')
+    print(df_cliente.columns)
     new_row = pd.DataFrame({'nome': cliente.nome, 'contato': cliente.contato,
                             'total_comprado': cliente.total_comprado,
                             'total_pago': cliente.total_pago}, index=[0])
     # simply concatenate both dataframes
-    df = pd.concat([df, new_row]).reset_index(drop=True)
-    df = df.dropna(axis=1)
-    return df
+    df_cliente = pd.concat([df_cliente, new_row]).reset_index(drop=True)
+    # df_cliente = df_cliente.dropna(axis=1)
+    # save to csv
+    df_cliente.to_csv("armazem/clientes.csv")
+    # calls inserir compras
+    id_cliente = df_cliente[df_cliente['nome'] == cliente.nome].index
+    inserir_rows_compra(cliente.compras, id_cliente)
+
+    # calls inserir pagamentos
+    inserir_rows_pagamento(cliente.pagamentos, id_cliente)
+    return df_cliente
 
 
-def gerar_dados_aleatorios():
+def inserir_rows_compra(compras, id_cliente):
+    df_compra = pd.read_csv('armazem/compras.csv')
+    for comp in compras:
+        new_row = pd.DataFrame({'id_cliente': id_cliente, 'nome_produto': comp.produto.nome,
+                                'valor_produto': comp.produto.valor,
+                                'descricao_produto': comp.produto.descricao, 'data': comp.data,
+                                'is_debito_imediato': comp.is_debito_imediato}, index=[0])
+        df_compra = pd.concat([df_compra, new_row]).reset_index(drop=True)
+    df_compra.dropna(axis=1)
+    df_compra.to_csv("armazem/compras.csv")
+
+    return df_compra
+
+
+def inserir_rows_pagamento(pagamentos, id_cliente):
+    df_pagamento = pd.read_csv("armazem/pagamentos.csv")
+    for pag in pagamentos:
+        new_row = pd.DataFrame({'id_cliente': id_cliente, 'valor': pag.valor,
+                                'data': pag.data}, index=[0])
+        df_pagamento = pd.concat([df_pagamento, new_row]).reset_index(drop=True)
+    df_pagamento.dropna(axis=1)
+    df_pagamento.to_csv("armazem/pagamentos.csv")
+    return df_pagamento
+
+
+def gerar_cliente_aleatorio(seed):
     data_de_hoje = date.today()
-    prod1 = Produto('vassoura', 20, 'varre bem')
+    prod1 = Produto('vassoura', 20*seed, 'varre bem')
     prod2 = Produto('pá', 2, 'é boa mas tá furada')
-    cleitim = Cliente('alex', 40028922)
+    cleitim = Cliente(f'alex {seed}', 40028922)
     cleitim.comprar(Compra(prod1, data_de_hoje))
     cleitim.comprar(Compra(prod2, data_de_hoje))
-    pag1 = Pagamento(15, data_de_hoje)
+    pag1 = Pagamento(15*seed, data_de_hoje)
     cleitim.pagar(pag1)
     return cleitim
 
 
+def gerar_compra_aleatoria():
+    pass
+
+
+def gerar_pagamento_aleatorio():
+    pass
